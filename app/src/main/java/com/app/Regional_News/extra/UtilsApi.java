fb278fbcb2e7e2a881f69460973c2868d0be787e
TTHPATEL <thchhbhaiya@gmail.com>
2024-06-24 16:04:37 +0530

    new ui design

--- app/src/main/java/com/app/Regional_News/extra/UtilsApi.java
+++ app/src/main/java/com/app/Regional_News/extra/UtilsApi.java
@@ -2,7 +2,7 @@ package com.app.Regional_News.extra;
 
 public class UtilsApi {
     public static final String BASE_URL_API = "http://192.168.0.196/KutchToday/";
-    public static final String BASE_URL_APINgo = "http://192.168.29.117/ndss/";
+//    public static final String BASE_URL_APINgo = "http://192.168.29.117/ndss/";
 
     //  Interface BaseApiService
     public static BaseApiService getAPIService(){
