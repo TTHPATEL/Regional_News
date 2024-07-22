2ad466b63ddb6819747ad937bc76cc246b0291e0
TTHPATEL <thchhbhaiya@gmail.com>
2024-07-22 23:39:40 +0530

    New color palette design

--- app/src/main/java/com/app/Regional_News/ui/NewsShowActivity.java
+++ app/src/main/java/com/app/Regional_News/ui/NewsShowActivity.java
@@ -128,7 +128,7 @@ public class NewsShowActivity extends AppCompatActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             // your code
-            return true;
+            return super.onKeyDown(keyCode, event);
         }
 
         return super.onKeyDown(keyCode, event);
