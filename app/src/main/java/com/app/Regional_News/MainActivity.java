c1d1b90bd2319facf33607e4eadc4234a1d41f53
TTHPATEL <thchhbhaiya@gmail.com>
2024-07-23 22:59:13 +0530

    Toolbar redesign

--- app/src/main/java/com/app/Regional_News/MainActivity.java
+++ app/src/main/java/com/app/Regional_News/MainActivity.java
@@ -1,71 +1,52 @@
 package com.app.Regional_News;
 
-import static com.app.Regional_News.extra.UtilsApi.BASE_URL_API;
-
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.app.Regional_News.data.Udata;
 import com.app.Regional_News.extra.SharedPrefManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
+import com.google.gson.Gson;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.app.Regional_News.databinding.ActivityMainBinding;
-import com.google.gson.Gson;
-import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+    private NavController navController;
     SharedPrefManager sharedPrefManager;
     Udata fp;
     public  static  String uid;
-    NavController navController;
-
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Ensure this points to the correct layout file
+
+
         sharedPrefManager = new SharedPrefManager(this);
         String fdata=sharedPrefManager.getFdata();
         Gson gson = new Gson();
         fp = gson.fromJson(fdata, Udata.class);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-//        binding.appBarMain.fab.setOnClickListener(view ->
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show());
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(findViewById(R.id.toolbar)); // Ensure this ID matches your layout
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_event, R.id.nav_setting)
                 .build();
 
-
-
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);  // Activity_main ma jee Framelayout che aaa enu id select kare che
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
-
     }
 
     @Override
@@ -76,11 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-
-
-
 }
