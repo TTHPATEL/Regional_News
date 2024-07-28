67ba784195ed129a9dc1a00237386232893d7740
TTHPATEL <thchhbhaiya@gmail.com>
2024-07-28 11:30:43 +0530

    NEW CMT

--- app/src/main/java/com/app/Regional_News/extra/UtilsApi.java
+++ app/src/main/java/com/app/Regional_News/extra/UtilsApi.java
@@ -2,7 +2,7 @@ package com.app.Regional_News.extra;
 
 public class UtilsApi {
     public static final String BASE_URL_API = "http://192.168.0.194/KutchToday/";
-//    public static final String BASE_URL_APINgo = "http://192.168.29.117/ndss/"; TOOLBARSS  UPDATED 1
+//    public static final String BASE_URL_APINgo = "http://192.168.29.117/ndss/"; TOOLBARSS  UPDATED 1234567890
 
     //  Interface BaseApiService
     public static BaseApiService getAPIService(){
