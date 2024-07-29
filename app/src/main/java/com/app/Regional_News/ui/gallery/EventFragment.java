07e1d5ad727c50d63e34917e1f48de191bfc76f0
TTHPATEL <thchhbhaiya@gmail.com>
2024-07-29 23:31:51 +0530

    searchbar's keyword come from databse

--- app/src/main/java/com/app/Regional_News/ui/gallery/EventFragment.java
+++ app/src/main/java/com/app/Regional_News/ui/gallery/EventFragment.java
@@ -11,22 +11,16 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.Regional_News.R;
-import com.app.Regional_News.adapter.EventAdapter;
-import com.app.Regional_News.data.Eventdata;
-import com.app.Regional_News.data.Eventlistdata;
+import com.app.Regional_News.data.Keywords_fetch_data;
+import com.app.Regional_News.data.Keywords_fetch_listdata;
 import com.app.Regional_News.extra.BaseApiService;
-import com.app.Regional_News.extra.ItemOffsetDecoration;
 import com.app.Regional_News.extra.NetworkUtils;
 import com.app.Regional_News.extra.UtilsApi;
 
@@ -36,58 +30,29 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-
 public class EventFragment extends Fragment {
-//    public RecyclerView recyclerView;
-//    private ProgressBar progressBar;
-//    private LinearLayout lyt_not_found;
-    BaseApiService mApiService;
-    EventAdapter adapter;
 
+    BaseApiService mApiService;
     private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
-    private String[] name = {"Stock Market", "Cricket", "UG Result", "Budget", "Civil Services", "Politics", "Scholarship", "Education", "Banking Sector", "Pharma Sector", "Olympics", "Electronics", "Business", "Social Media"};
-
     private LinearLayout searchLinearLayout;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_event, container, false);
         mApiService = UtilsApi.getAPIService();
 
-//        lyt_not_found = rootView.findViewById(R.id.lyt_not_found);
-//        progressBar = rootView.findViewById(R.id.progressBar);
-//        recyclerView = rootView.findViewById(R.id.recyclerView);
-//        recyclerView.setHasFixedSize(true);
-
         searchLinearLayout = rootView.findViewById(R.id.searchlinearlayout);
-
-
         listView = rootView.findViewById(R.id.listview);
-        arrayAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, name);
+
+        // Initialize ArrayAdapter with empty data
+        arrayAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, new ArrayList<>());
         listView.setAdapter(arrayAdapter);
 
         setHasOptionsMenu(true); // Important to notify that the fragment has its own menu
 
-
-
-//        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),2);
-//        gridLayoutManager.setOrientation(LinearLayoutManager.VERTICAL); // set Horizontal Orientation
-//        recyclerView.setLayoutManager(gridLayoutManager); // set LayoutManager to RecyclerView
-
-
-
-
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
-//        ItemOffsetDecoration itemDecoration = new ItemOffsetDecoration(requireActivity(), R.dimen.item_offset);
-//        recyclerView.addItemDecoration(itemDecoration);
-//        ItemOffsetDecoration itemDecoration = new ItemOffsetDecoration(requireActivity(), R.dimen.item_offset);
-//        recyclerView.addItemDecoration(itemDecoration);
-//        recyclerView.addItemDecoration(new DividerItemDecoration(this.getActivity(), LinearLayout.VERTICAL));
         if (NetworkUtils.isConnected(getActivity())) {
-//            showProgress(true);
-//            getevent();
+            fetchKeywords();
         } else {
             Toast.makeText(getActivity(), getString(R.string.conne_msg1), Toast.LENGTH_SHORT).show();
         }
@@ -95,14 +60,46 @@ public class EventFragment extends Fragment {
         return rootView;
     }
 
+    private void fetchKeywords() {
+        mApiService.rnKeywordRequest("keyword_fetch")
+                .enqueue(new Callback<Keywords_fetch_data>() {
+                    @Override
+                    public void onResponse(Call<Keywords_fetch_data> call, Response<Keywords_fetch_data> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            Keywords_fetch_data data = response.body();
+                            if (data.getStatus().equals("1")) {
+                                ArrayList<Keywords_fetch_listdata> keywords = data.getKeyword_fetch_list();
+                                if (keywords != null) {
+                                    ArrayList<String> keywordStrings = new ArrayList<>();
+                                    for (Keywords_fetch_listdata keywordData : keywords) {
+                                        keywordStrings.add(keywordData.getKeyword());
+                                    }
+                                    arrayAdapter.clear();
+                                    arrayAdapter.addAll(keywordStrings);
+                                    arrayAdapter.notifyDataSetChanged();
+                                } else {
+                                    Toast.makeText(getActivity(), "No keywords found", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(getActivity(), data.getMsg(), Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Response Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Keywords_fetch_data> call, Throwable t) {
+                        Toast.makeText(getActivity(), "API Call Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.event_menu, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) searchItem.getActionView();
-
-
 
         searchView.setOnSearchClickListener(new View.OnClickListener() {
             @Override
@@ -129,9 +126,6 @@ public class EventFragment extends Fragment {
             }
         });
 
-
-
-
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -153,62 +147,8 @@ public class EventFragment extends Fragment {
 
     private void performSearch(String query) {
         Toast.makeText(requireContext(), "Search query: " + query, Toast.LENGTH_SHORT).show();
-
     }
 
-
-    //    private void getevent() {
-//        mApiService.dsseventRequest("event_list")
-//                .enqueue(new Callback<Eventdata>() {
-//                    @Override
-//                    public void onResponse(Call<Eventdata> call, Response<Eventdata> response) {
-//                        if (response.isSuccessful()){
-//                            Log.e("msg",""+response.code());
-//                            showProgress(false);
-//                            Eventdata degdata=response.body();
-//                            Log.e("msg2",degdata.getMsg());
-//                            if (degdata.getStatus().equals("1")){
-//                                String error_message = degdata.getMsg();
-//                                Toast.makeText(getActivity(), error_message, Toast.LENGTH_SHORT).show();
-//                                displayData(degdata.getEvent_list());
-//                            } else {
-//                                String error_message = degdata.getMsg();
-//                                Toast.makeText(getActivity(), error_message, Toast.LENGTH_SHORT).show();
-//                            }
-//                        } else {
-//                            Log.e("msg1",""+response.code());
-//                            Log.e("msg5",""+call.request().url());
-//                            showProgress(false);
-//                        }
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<Eventdata> call, Throwable t) {
-//                        Log.e("debug", "onFailure: ERROR > " + t.toString());
-//                        showProgress(false);
-//                    }
-//                });
-//    }
-//    private void showProgress(boolean show) {
-//        if (show) {
-//            progressBar.setVisibility(View.VISIBLE);
-//            recyclerView.setVisibility(View.GONE);
-//            lyt_not_found.setVisibility(View.GONE);
-//        } else {
-//            progressBar.setVisibility(View.GONE);
-//            recyclerView.setVisibility(View.VISIBLE);
-//        }
-//    }
-//    private void displayData(ArrayList<Eventlistdata> degree_list) {
-//        adapter = new EventAdapter(getActivity(), degree_list);
-//        recyclerView.setAdapter(adapter);
-//
-//        if (adapter.getItemCount() == 0) {
-//            lyt_not_found.setVisibility(View.VISIBLE);
-//        } else {
-//            lyt_not_found.setVisibility(View.GONE);
-//        }
-//    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
