7df5bb7ac7cbe7a1a100317bc9323f79efc60753
TTHPATEL <thchhbhaiya@gmail.com>
2024-07-29 22:12:59 +0530

    toolbar searchbar is integrated v2

--- app/src/main/java/com/app/Regional_News/MainActivity.java
+++ app/src/main/java/com/app/Regional_News/MainActivity.java
@@ -3,9 +3,6 @@ package com.app.Regional_News;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewStub;
 import android.widget.Toast;
 
 import com.app.Regional_News.data.Udata;
@@ -14,7 +11,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.gson.Gson;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -27,24 +23,21 @@ public class MainActivity extends AppCompatActivity {
     private NavController navController;
     SharedPrefManager sharedPrefManager;
     Udata fp;
-    public  static  String uid;
+    public static String uid;
     private Toolbar toolbar;
-    private SearchView searchView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Ensure this points to the correct layout file
-
+        setContentView(R.layout.activity_main);
 
         sharedPrefManager = new SharedPrefManager(this);
-        String fdata=sharedPrefManager.getFdata();
+        String fdata = sharedPrefManager.getFdata();
         Gson gson = new Gson();
         fp = gson.fromJson(fdata, Udata.class);
 
         toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar); // Ensure this ID matches your layout
+        setSupportActionBar(toolbar);
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_event, R.id.nav_setting)
@@ -56,70 +49,31 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-
-
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
             if (destination.getId() == R.id.nav_home) {
                 toolbar.setTitle("Regional News");
                 toolbar.setLogo(R.drawable.icon_news);
-                toolbar.getMenu().clear();
-                getMenuInflater().inflate(R.menu.main, toolbar.getMenu());
             } else if (destination.getId() == R.id.nav_event) {
-                toolbar.setTitle("");
+                toolbar.setTitle("Search");
                 toolbar.setLogo(null);
-                toolbar.getMenu().clear();
-
-                getMenuInflater().inflate(R.menu.event_menu, toolbar.getMenu());
-                MenuItem searchItem = toolbar.getMenu().findItem(R.id.action_search);
-                searchView = (SearchView) searchItem.getActionView();
-
-//                searchItem.expandActionView();
-//                searchView.requestFocus();
-                // Set up the SearchView listener
-                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextSubmit(String query) {
-                        // Perform search operation with the query
-                        performSearch(query);
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onQueryTextChange(String newText) {
-                        // Update search suggestions as the user types
-                        updateSuggestions(newText);
-                        return false;
-                    }
-                });
-
-
-//                getMenuInflater().inflate(R.menu.event_menu, toolbar.getMenu());
-//                View searchView = toolbar.getMenu().findItem(R.id.action_search).getActionView();
-//                // Add listeners or any additional setup for searchView
             } else if (destination.getId() == R.id.nav_setting) {
                 toolbar.setTitle("Setting");
                 toolbar.setLogo(null);
-                toolbar.getMenu().clear();
             }
+            invalidateOptionsMenu(); // Request to recreate the options menu
         });
-
-
-    }
-
-    private void performSearch(String query) {
-        // Implement search functionality here
-        // For example, query the database and show results
-        Toast.makeText(this, "Search query: " + query, Toast.LENGTH_SHORT).show();
-    }
-
-    private void updateSuggestions(String query) {
-        // Implement suggestion fetching from the database here
-        // For example, update search suggestions dynamically
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        int currentFragmentId = navController.getCurrentDestination().getId();
+        if (currentFragmentId == R.id.nav_home) {
+            getMenuInflater().inflate(R.menu.main, menu);
+        } else if (currentFragmentId == R.id.nav_event) {
+            // Do not inflate any menu here, as the fragment will handle it
+        } else if (currentFragmentId == R.id.nav_setting) {
+            // Inflate the settings menu if needed
+        }
         return true;
     }
 
--- app/src/main/java/com/app/Regional_News/ui/gallery/EventFragment.java
+++ app/src/main/java/com/app/Regional_News/ui/gallery/EventFragment.java
@@ -3,13 +3,19 @@ package com.app.Regional_News.ui.gallery;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -32,92 +38,144 @@ import retrofit2.Response;
 
 
 public class EventFragment extends Fragment {
-    public RecyclerView recyclerView;
-    private ProgressBar progressBar;
-    private LinearLayout lyt_not_found;
+//    public RecyclerView recyclerView;
+//    private ProgressBar progressBar;
+//    private LinearLayout lyt_not_found;
     BaseApiService mApiService;
     EventAdapter adapter;
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private String[] name = {"Stock Market", "Cricket", "UG Result", "Budget", "Civil Services", "Politics", "Scholarship", "Education", "Banking Sector", "Pharma Sector", "Olympics", "Electronics", "Business", "Social Media"};
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_event, container, false);
         mApiService = UtilsApi.getAPIService();
 
-        lyt_not_found = rootView.findViewById(R.id.lyt_not_found);
-        progressBar = rootView.findViewById(R.id.progressBar);
-        recyclerView = rootView.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
+//        lyt_not_found = rootView.findViewById(R.id.lyt_not_found);
+//        progressBar = rootView.findViewById(R.id.progressBar);
+//        recyclerView = rootView.findViewById(R.id.recyclerView);
+//        recyclerView.setHasFixedSize(true);
+
+
+        listView = rootView.findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, name);
+        listView.setAdapter(arrayAdapter);
+
+        setHasOptionsMenu(true); // Important to notify that the fragment has its own menu
+
+
+
 //        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),2);
 //        gridLayoutManager.setOrientation(LinearLayoutManager.VERTICAL); // set Horizontal Orientation
 //        recyclerView.setLayoutManager(gridLayoutManager); // set LayoutManager to RecyclerView
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+
+
+
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
 //        ItemOffsetDecoration itemDecoration = new ItemOffsetDecoration(requireActivity(), R.dimen.item_offset);
 //        recyclerView.addItemDecoration(itemDecoration);
-        ItemOffsetDecoration itemDecoration = new ItemOffsetDecoration(requireActivity(), R.dimen.item_offset);
-        recyclerView.addItemDecoration(itemDecoration);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this.getActivity(), LinearLayout.VERTICAL));
+//        ItemOffsetDecoration itemDecoration = new ItemOffsetDecoration(requireActivity(), R.dimen.item_offset);
+//        recyclerView.addItemDecoration(itemDecoration);
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this.getActivity(), LinearLayout.VERTICAL));
         if (NetworkUtils.isConnected(getActivity())) {
-            showProgress(true);
-            getevent();
+//            showProgress(true);
+//            getevent();
         } else {
             Toast.makeText(getActivity(), getString(R.string.conne_msg1), Toast.LENGTH_SHORT).show();
         }
 
         return rootView;
     }
-    private void getevent() {
-        mApiService.dsseventRequest("event_list")
-                .enqueue(new Callback<Eventdata>() {
-                    @Override
-                    public void onResponse(Call<Eventdata> call, Response<Eventdata> response) {
-                        if (response.isSuccessful()){
-                            Log.e("msg",""+response.code());
-                            showProgress(false);
-                            Eventdata degdata=response.body();
-                            Log.e("msg2",degdata.getMsg());
-                            if (degdata.getStatus().equals("1")){
-                                String error_message = degdata.getMsg();
-                                Toast.makeText(getActivity(), error_message, Toast.LENGTH_SHORT).show();
-                                displayData(degdata.getEvent_list());
-                            } else {
-                                String error_message = degdata.getMsg();
-                                Toast.makeText(getActivity(), error_message, Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Log.e("msg1",""+response.code());
-                            Log.e("msg5",""+call.request().url());
-                            showProgress(false);
-                        }
-                    }
 
-                    @Override
-                    public void onFailure(Call<Eventdata> call, Throwable t) {
-                        Log.e("debug", "onFailure: ERROR > " + t.toString());
-                        showProgress(false);
-                    }
-                });
-    }
-    private void showProgress(boolean show) {
-        if (show) {
-            progressBar.setVisibility(View.VISIBLE);
-            recyclerView.setVisibility(View.GONE);
-            lyt_not_found.setVisibility(View.GONE);
-        } else {
-            progressBar.setVisibility(View.GONE);
-            recyclerView.setVisibility(View.VISIBLE);
-        }
-    }
-    private void displayData(ArrayList<Eventlistdata> degree_list) {
-        adapter = new EventAdapter(getActivity(), degree_list);
-        recyclerView.setAdapter(adapter);
 
-        if (adapter.getItemCount() == 0) {
-            lyt_not_found.setVisibility(View.VISIBLE);
-        } else {
-            lyt_not_found.setVisibility(View.GONE);
-        }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.event_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Perform search operation with the query
+                performSearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Update search suggestions as the user types
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
+
+    private void performSearch(String query) {
+        Toast.makeText(requireContext(), "Search query: " + query, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    //    private void getevent() {
+//        mApiService.dsseventRequest("event_list")
+//                .enqueue(new Callback<Eventdata>() {
+//                    @Override
+//                    public void onResponse(Call<Eventdata> call, Response<Eventdata> response) {
+//                        if (response.isSuccessful()){
+//                            Log.e("msg",""+response.code());
+//                            showProgress(false);
+//                            Eventdata degdata=response.body();
+//                            Log.e("msg2",degdata.getMsg());
+//                            if (degdata.getStatus().equals("1")){
+//                                String error_message = degdata.getMsg();
+//                                Toast.makeText(getActivity(), error_message, Toast.LENGTH_SHORT).show();
+//                                displayData(degdata.getEvent_list());
+//                            } else {
+//                                String error_message = degdata.getMsg();
+//                                Toast.makeText(getActivity(), error_message, Toast.LENGTH_SHORT).show();
+//                            }
+//                        } else {
+//                            Log.e("msg1",""+response.code());
+//                            Log.e("msg5",""+call.request().url());
+//                            showProgress(false);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<Eventdata> call, Throwable t) {
+//                        Log.e("debug", "onFailure: ERROR > " + t.toString());
+//                        showProgress(false);
+//                    }
+//                });
+//    }
+//    private void showProgress(boolean show) {
+//        if (show) {
+//            progressBar.setVisibility(View.VISIBLE);
+//            recyclerView.setVisibility(View.GONE);
+//            lyt_not_found.setVisibility(View.GONE);
+//        } else {
+//            progressBar.setVisibility(View.GONE);
+//            recyclerView.setVisibility(View.VISIBLE);
+//        }
+//    }
+//    private void displayData(ArrayList<Eventlistdata> degree_list) {
+//        adapter = new EventAdapter(getActivity(), degree_list);
+//        recyclerView.setAdapter(adapter);
+//
+//        if (adapter.getItemCount() == 0) {
+//            lyt_not_found.setVisibility(View.VISIBLE);
+//        } else {
+//            lyt_not_found.setVisibility(View.GONE);
+//        }
+//    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
