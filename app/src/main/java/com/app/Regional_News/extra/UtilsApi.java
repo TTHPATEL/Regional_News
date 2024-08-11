85d1f211c7c7d9f47cf480a7dbfe00023f7462f4
TTHPATEL <thchhbhaiya@gmail.com>
2024-08-11 20:37:18 +0530

    Profile Updation- data enter in db

--- app/src/main/java/com/app/Regional_News/extra/UtilsApi.java
+++ app/src/main/java/com/app/Regional_News/extra/UtilsApi.java
@@ -3,7 +3,8 @@ package com.app.Regional_News.extra;
 public class UtilsApi {
     //    public static final String BASE_URL_API = "http://192.168.218.179/KutchToday/";  // TIRTH PHONE
 //    public static final String BASE_URL_API = "http://192.168.243.178/KutchToday/"; // MUMMY PHONE
-    public static final String BASE_URL_API = "http://192.168.0.197/KutchToday/"; // 5G WIFI ROUTER
+    public static final String BASE_URL_API = "http://192.168.0.195/KutchToday/"; // 5G WIFI ROUTER
+
 
     //  Interface BaseApiService
     public static BaseApiService getAPIService(){
